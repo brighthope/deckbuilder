d0ec3abc88439d72f0429d4ff3407d66ee4641f8
Cedrik Vanderhaegen <cedrik.vanderhaegen@geosolutions.be>
2016-04-07 20:29:22 +0200

    add deck list images to pdf

--- client/src/app/util/PdfUtil.js
+++ client/src/app/util/PdfUtil.js
@@ -30,33 +30,56 @@ define([
       return doc;
     },
 
+    _createPath: function (wrongPath) {
+      var imageSet = wrongPath.split('/')[2];
+      var imageName = wrongPath.split('/')[3];
+      imageName = imageName.substring(2);
+      return '/cards/starwars/' + imageSet + '/large/' + imageName + '.gif';
+    },
+
+    _createBackPath: function (wrongPath) {
+      var imageSet = wrongPath.split('/')[2];
+      var imageNameBack = wrongPath.split('/')[3].substring(2);
+      return '/cards/starwars/' + imageSet + '/large/' + imageNameBack + '.gif';
+    },
+
     createImages: function (deck) {
+      var deferred = new Deferred();
       //var pdf = this._getImageFromUrl('/cards/starwars/ANewHope-Dark/large/advosze.gif', this._createImagePdf);
       // To configure the loader, pass an options hash as the first parameter.
-      var doc = new jsPDF();
-      var deferred = new Deferred();
+      var doc = new jsPDF('portrait', 'mm', 'a4');
+      var imageList = [];
+      deck.fetchSync().forEach(function(card) {
+        var copies;
+        for (copies = 0; copies < card.quantity; copies++) {
+          imageList.push(this._createPath(card.image_path));
+          if (card.image_path_back) {
+            imageList.push(this._createBackPath(card.image_path_back));
+          }
+        }
+      }, this);
 
+
+      var cardWidth = 63;
+      var cardHeight = 88;
       imageLoad({
         // a src root for relative URLs. May itself be relative or absolute.
         srcRoot: '/app/resources',
 
         // Attributes applied to each image when not already specified for the image
         defaultAttributes: {
-          width: 24,
-          height: 24
+          //width: cardWidth,
+          //height: cardHeight
         }
-      },[
-        '/cards/starwars/ANewHope-Dark/large/advosze.gif',
-        '/cards/starwars/ANewHope-Dark/large/advosze.gif'
-      ]).then(function(imageArray){
+      }, imageList).then(function(imageArray){
         // Enjoy an array of uniformly-sized images.
-        var x = 0,
-          y = 0;
+        var x = 10,
+          y = 10;
 
         array.forEach((imageArray), function (image) {
-          doc.addImage(image, 'JPEG', x, y);
-          x += 30;
-          y += 30;
+          doc.addImage(image, 'JPEG', x, y, cardWidth, cardHeight);
+          x += cardWidth;
+          y += 0;
         });
 
         deferred.resolve(doc);
